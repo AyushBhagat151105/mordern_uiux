3eb7acf8465ebb43f411fcac25c2421fb87cf1e3
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-08-15 23:43:29 +0530

    first commit

---
+++ src/components/Billing.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const Billing = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      "#product",
+      {
+        x: 800,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        delay: 6,
+        
+        scrollTrigger: {
+          trigger: "#product",
+          start: " bottom top",
+          end: "center center",
+
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+  return (
+    <selection id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" /> billing &
+          invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt="apple_store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google_store"
+            lassName="w-[128px] h-[42px] object-contain cursor-pointer"
+          />
+        </div>
+      </div>
+    </selection>
+  );
+};
+
+export default Billing;
---
+++ src/components/Clients.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "../style";
+import { clients } from "../constants";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const Clients = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      "#clients",
+      {
+        x: 800,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        delay: 6,
+
+        scrollTrigger: {
+          trigger: "#clients",
+          start: " bottom top",
+          end: "center center",
+
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+  return (
+    <section id="clients" className={`${styles.flexCenter} my-4`}>
+      <div
+        className={`${styles.flexCenter} felx-wrap w-full sm:flex-row flex-col sm:scale-100 scale-70`}
+      >
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 flex ${styles.flexCenter} sm:min-w[192px] min-w-[120px] sm:mb-0 mb-10`}
+          >
+            <img
+              src={client.logo}
+              alt={client.name}
+              className="sm:w-[192px] w-[100%] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Clients;
